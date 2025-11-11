0ad2e1b5c3eb1f44db1db17815f988caae0ac15f
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2025-11-10 21:02:05 -0500

    Subiendo cita Repository Impl

---
+++ src/main/java/com/MediTrack/persistance/repositoryimpl/AppointmentRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.MediTrack.persistance.repositoryimpl;
+
+import com.MediTrack.domain.repository.AppointmentRepository;
+import com.MediTrack.persistance.crud.AppointmentCrudRepository;
+import com.MediTrack.persistance.entity.Appointment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AppointmentRepositoryImpl implements AppointmentRepository {
+
+    @Autowired
+    private AppointmentCrudRepository crud;
+
+    @Override
+    public Appointment save(Appointment appointment) {
+        return crud.save(appointment);
+    }
+
+    @Override
+    public Optional<Appointment> findById(Long id) {
+        return crud.findById(id);
+    }
+
+    @Override
+    public List<Appointment> findByPacienteId(Long pacienteId) {
+        return crud.findByPacienteId(pacienteId);
+    }
+
+    @Override
+    public List<Appointment> findByMedicoId(Long medicoId) {
+        return crud.findByMedicoId(medicoId);
+    }
+
+    @Override
+    public List<Appointment> findByEstado(String estado) {
+        return crud.findByEstado(estado);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        crud.deleteById(id);
+    }
+
+    @Override
+    public long count() {
+        return crud.count();
+    }
+}
