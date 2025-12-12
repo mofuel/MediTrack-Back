86bd83cf06732370aece4169e6fb25d81f8e9191
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2025-12-11 20:36:00 -0500

    Modificando la url del WebConfig

--- src/main/java/com/MediTrack/config/WebConfig.java
+++ src/main/java/com/MediTrack/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://meditrack-front.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
